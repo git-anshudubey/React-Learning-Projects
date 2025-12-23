b66d2cf8969671dbff572a7256c75dc4926ae52f
git-anshudubey <6152anshudubey@gmail.com>
2025-12-23 08:47:24 +0530

    ReactRoter Comp

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,19 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './compoments/Header/Header'
-import Footer from './compoments/Footer/Footer'
-import Home from './compoments/Home/Home'
 
 function App() {
 
 
   return (
-    <>
-      {/* <p className='bg-green-600 p-4 text-black'>Hello to React Router.</p> */}
-      <Header/>
-      <Home/>
-      <Footer/>
-    </>
+   <>
+   </>
   )
 }
 
